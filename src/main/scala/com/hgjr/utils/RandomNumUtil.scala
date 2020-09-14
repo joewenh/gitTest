e384e839b0b2b5218b6d887ce8db82aa2fbd5555
Administrator <joe_wenh@163.com>
2020-09-14 21:29:11 +0800

    2增加亿条输出

--- src/main/scala/com/hgjr/utils/RandomNumUtil.scala
+++ src/main/scala/com/hgjr/utils/RandomNumUtil.scala
@@ -36,23 +36,5 @@ object RandomNumUtil {
   def main(args: Array[String]): Unit = {
     println(randomMultiInt(1, 15, 10))
     println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
-    println(randomMultiInt(1, 8, 10, false))
   }
 }
