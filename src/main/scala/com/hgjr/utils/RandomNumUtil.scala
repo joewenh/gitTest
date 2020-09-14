5c3e6fe2b3c1e7e571d61fb75f6f0e98fd335164
Administrator <joe_wenh@163.com>
2020-09-14 17:28:52 +0800

    3增加亿条输出

--- src/main/scala/com/hgjr/utils/RandomNumUtil.scala
+++ src/main/scala/com/hgjr/utils/RandomNumUtil.scala
@@ -38,5 +38,6 @@ object RandomNumUtil {
     println(randomMultiInt(1, 8, 10, false))
     println(randomMultiInt(1, 8, 10, false))
     println(randomMultiInt(1, 8, 10, false))
+    println(randomMultiInt(1, 8, 10, false))
   }
 }
