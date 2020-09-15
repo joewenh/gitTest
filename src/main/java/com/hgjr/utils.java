c2313db123b4ee705656c79d62c3dfa2e2944483
Administrator <joe_wenh@163.com>
2020-09-15 10:46:50 +0800

    4

--- src/main/java/com/hgjr/utils.java
+++ src/main/java/com/hgjr/utils.java
@@ -3,5 +3,8 @@ package com.hgjr;
 public class utils {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
